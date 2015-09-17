50b7fa106481780d0853af4dea9415d1289ddd4f
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-17 12:58:52 -0600

    add idea model tests for presence, uniqueness

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,7 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
+  
+  validates :title, :body, :user_id, presence: true,
+                                     uniqueness: true
 end
 
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid" do
+    idea = Idea.new(title: "first idea", body: "this is th boooody", user_id: 1)
+    expect(idea).to be_valid
+  end
+
+  it "is not valid without title" do
+    idea = Idea.new(body: "this is th boooody")
+    expect(idea).to_not be_valid
+  end
+
+  it "is not valid without body" do
+    idea = Idea.new(title: "title")
+    expect(idea).to_not be_valid
+  end
+
+  it "is not valid without user_id" do
+    idea = Idea.new(title: "title", body: "booody")
+    expect(idea).to_not be_valid
+  end
+
+  it "cannot save duplicate usernames" do
+    Idea.create(title: "first idea", body: "this is th boooody", user_id: 1)
+    idea = Idea.new(title: "first idea", body: "this is th boooody", user_id: 1)
+    expect(idea).to_not be_valid 
+  end
 end
