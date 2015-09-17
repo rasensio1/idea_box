fc15f671093b3bdfc124e53b332316aabfd6b48d
Ryan Asensio <ryan.asensio@gmail.com>
2015-09-17 13:55:46 -0600

    add model validataions for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid' do
+    category = Category.new(name: "Life" )
+    expect(category).to be_valid
+  end
+
+  it 'is not valid without name' do
+    category = Category.new()
+    expect(category).to_not be_valid
+  end
 end
